8eca66abeb3f7e70ac6bb2a45ec3907c566aa650
tristancable <tcable@student.neumont.edu>
2024-08-05 11:23:43 -0600

    node_modules, frontend, header, index, package, site.css all changed/added

---
+++ public/js/frontend.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const port = 1224;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.listen(port, () => {
+    console.log(`Website Game Launcher running on http://localhost:${port}/`);
+});
