1b21dacda56b74f274aecc60aa5072d347e440eb
ezeklaww <eandreason@student.neumont.edu>
2024-08-06 11:50:43 -0600

    updated minesweeper to have the basic clicking functionallity

--- public/js/minesweeper.js
+++ public/js/minesweeper.js
@@ -1,9 +1,37 @@
 //minesweeper functions
+const EASY = 5;
+let remianingMineCount = 0;
 
 window.onload = function() {
     setGame();
 }
 
 function setGame() {
+    generateTiles();
+
+}
+
+function generateTiles() {
+
+    for (let i = 0; i < EASY; i++) {
+        for (let j = 0; j < EASY; j++) {
+            let tile = document.createElement("button");
+            let idName = "tile" + i + j;
+            tile.id = idName;
+            tile.className = "tile";
+            tile.addEventListener("click", selectTile);
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
+
+function selectTile(){
+    let tempTile = this.id;
     
+    console.log(tempTile);
+
+    document.getElementById(tempTile).classList.add("tileBomb");
+
+    //document.getElementById(tempTile).classList.add("tileClear");
+
 }
