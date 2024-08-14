ab51bd21780cc98fcd7febb495d5cc5027ff9dd0
ItsLilyUwU <161492491+ItsLilyUwU@users.noreply.github.com>
2024-08-14 12:01:52 -0600

    Enemy Generator (Outline)

--- public/js/ttyd.js
+++ public/js/ttyd.js
@@ -1,5 +1,19 @@
 let HPMario = 0;
+let MarioX = 0;
+let MarioY = 0;
 let HPVivian = 0;
+let VivianX = 0;
+let VivianY = 0;
+
+let Enemy1 = "";
+let HPEnemy1 = 0;
+let sizeEnemy1 = 50;
+let Enemy2 = "";
+let HPEnemy2 = 0;
+let sizeEnemy2 = 50;
+let Enemy3 = "";
+let HPEnemy3 = 0;
+let sizeEnemy3 = 50;
 
 async function wait(milliseconds) // Uses async to wait the inputted amount of milliseconds
 {
@@ -18,6 +32,28 @@ function start()
 
     splash.classList.add("hide");
     setGame();
+}
+
+function enemyGen(enemyPool, whichEnemy)
+{
+    switch(enemyPool) // Please for the love of god if I forget to add breaks I will actually explode
+    {
+        case "Gloomba":
+            break;
+    }
+}
+
+function enemyGen(enemyNum, whichEnemy)
+{
+    switch(enemyNum)
+    {
+        case 1: // Enemy 1: Basic Gloomba
+            break;
+        case 2: // Enemy 2: Paragloomba
+            break;
+        case 3: // Enemy 3: Spiky Gloomba
+            break;
+    }
 }
 
 async function entrance(getId, howFar)
@@ -92,8 +128,33 @@ async function unveil()
 
 function update()
 {
+    // Display the HP of both Mario and Vivian.
     document.getElementById('marioHP').textContent = "Mario: " + HPMario;
     document.getElementById('vivianHP').textContent = "Vivian: " + HPVivian;
+
+    // Retrieve sprite positions
+    document.getElementById('mario').textContent 
+    document.getElementById('vivian').textContent 
+
+    document.getElementById('enemy1HP').textContent = HPEnemy1; 
+    if(HPEnemy1 < 1)
+        document.getElementById("enemy1").style.visibility = "hidden";
+    else
+        document.getElementById("enemy1").style.visibility = "visible";
+        document.getElementById("enemy1").style.width = sizeEnemy1;
+        document.getElementById("enemy1").style.height = sizeEnemy1;
+    document.getElementById('enemy2HP').textContent = HPEnemy2;
+    if(HPEnemy2 < 1)
+        document.getElementById("enemy2").style.visibility = "hidden";
+    else
+        document.getElementById("enemy2").style.visibility = "visible";
+    document.getElementById('enemy3HP').textContent = HPEnemy3;
+    if(HPEnemy3 < 1)
+        document.getElementById("enemy3").style.visibility = "hidden";
+    else
+        document.getElementById("enemy3").style.visibility = "visible";
+
+    // TODO: These values will be hard coded to Gloombas for now, they should be updated with variables later down the road.
 }
 
 async function setGame()
@@ -108,6 +169,5 @@ async function setGame()
     await wait(1000);
     hop("mario", 2);
     hop("vivian", 2);
-    
-    // Come back later!
+    // ! Come back later!
 }
