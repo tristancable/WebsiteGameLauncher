988c557f01615fca568373a5239ce92f4c105663
ItsLilyUwU <161492491+ItsLilyUwU@users.noreply.github.com>
2024-08-19 10:10:20 -0600

    Functional Enemy Gen

--- public/js/ttyd.js
+++ public/js/ttyd.js
@@ -1,19 +1,44 @@
+/** Manually setting variable types (This is to make functions more reusable)
+ * @param {number} enemyNum
+ * @param {string} enemyPool
+ */
+
+let MarioAlive = true;
 let HPMario = 0;
 let MarioX = 0;
 let MarioY = 0;
+
+let VivianAlive = true;
 let HPVivian = 0;
 let VivianX = 0;
 let VivianY = 0;
 
-let Enemy1 = "";
+let enemyAlive1 = false;
+let enemyImg1 = "";
 let HPEnemy1 = 0;
-let sizeEnemy1 = 50;
-let Enemy2 = "";
+let ATKEnemy1 = 0;
+let sizeEnemy1 = 0;
+let groundPosEnemy1 = 0;
+let Enemy1XPos = 0;
+let Enemy1YPos = 0;
+
+let enemyAlive2 = false;
+let enemyImg2 = "";
 let HPEnemy2 = 0;
-let sizeEnemy2 = 50;
-let Enemy3 = "";
+let ATKEnemy2 = 0;
+let sizeEnemy2 = 0;
+let groundPosEnemy2 = 0;
+let Enemy2XPos = 0;
+let Enemy2YPos = 0;
+
+let enemyAlive3 = false;
+let enemyImg3 = "";
 let HPEnemy3 = 0;
-let sizeEnemy3 = 50;
+let ATKEnemy3 = 0;
+let sizeEnemy3 = 0;
+let groundPosEnemy3 = 0;
+let Enemy3XPos = 0;
+let Enemy3YPos = 0;
 
 async function wait(milliseconds) // Uses async to wait the inputted amount of milliseconds
 {
@@ -48,6 +73,30 @@ function enemyGen(enemyNum, whichEnemy)
     switch(enemyNum)
     {
         case 1: // Enemy 1: Basic Gloomba
+            switch(enemyNum)
+            {
+                case 1:
+                    sizeEnemy1 = 140;
+                    HPEnemy1 = 7;
+                    ATKEnemy1 = 3;
+                    groundPosEnemy1 = 565;
+                    enemyImg1 = "../assets/GloombaNormal.png";
+                    break;
+                case 2:
+                    sizeEnemy2 = 140;
+                    HPEnemy2 = 7;
+                    ATKEnemy2 = 3;
+                    groundPosEnemy2 = 565;
+                    enemyImg2 = "../assets/GloombaNormal.png";
+                    break;
+                case 3:
+                    sizeEnemy3 = 140;
+                    HPEnemy3 = 7;
+                    ATKEnemy3 = 3;
+                    groundPosEnemy3 = 565;
+                    enemyImg3 = "../assets/GloombaNormal.png";
+                    break;
+            }
             break;
         case 2: // Enemy 2: Paragloomba
             break;
@@ -64,6 +113,19 @@ async function entrance(getId, howFar)
     {
         
         document.getElementById(getId).style.left = `${i}px`;
+        i = i + 9;
+        await wait(10);
+    }
+}
+
+async function enemytrance(getId, howFar)
+{
+    document.getElementById(getId).style.visibility = "visible";
+    let i = 0;
+    while(i <= howFar)
+    {
+        
+        document.getElementById(getId).style.right = `${i}px`;
         i = i + 9;
         await wait(10);
     }
@@ -128,46 +190,72 @@ async function unveil()
 
 function update()
 {
+    // Ease of use
+    let infoMario =  document.getElementById("mario");
+    let infoVivian =  document.getElementById("vivian");
+    let infoEnemy1 = document.getElementById("enemy1");
+    let infoEnemy2 = document.getElementById("enemy2");
+    let infoEnemy3 = document.getElementById("enemy3");
+
     // Display the HP of both Mario and Vivian.
     document.getElementById('marioHP').textContent = "Mario: " + HPMario;
     document.getElementById('vivianHP').textContent = "Vivian: " + HPVivian;
 
     // Retrieve sprite positions
-    document.getElementById('mario').textContent 
-    document.getElementById('vivian').textContent 
+    MarioX = infoMario.width;
+    MarioY = infoMario.height;
+    VivianX = infoVivian.width;
+    VivianX = infoVivian.height;
 
-    document.getElementById('enemy1HP').textContent = HPEnemy1; 
+    // Utilize and update sprite variables
+    document.getElementById('enemyHP1').textContent = HPEnemy1; 
     if(HPEnemy1 < 1)
-        document.getElementById("enemy1").style.visibility = "hidden";
+        alive = false;
+        infoEnemy1.style.visibility = "hidden";
     else
-        document.getElementById("enemy1").style.visibility = "visible";
-        document.getElementById("enemy1").style.width = sizeEnemy1;
-        document.getElementById("enemy1").style.height = sizeEnemy1;
-    document.getElementById('enemy2HP').textContent = HPEnemy2;
+        alive = true;
+        infoEnemy1.style.width = `${sizeEnemy1}px`;
+        infoEnemy1.style.height = `${sizeEnemy1}px`;
+        infoEnemy1.style.top = `${groundPosEnemy1}px`;
+        infoEnemy1.src = enemyImg1;
+    infoEnemy2.textContent = HPEnemy2;
     if(HPEnemy2 < 1)
-        document.getElementById("enemy2").style.visibility = "hidden";
+        infoEnemy2.style.visibility = "hidden";
     else
-        document.getElementById("enemy2").style.visibility = "visible";
-    document.getElementById('enemy3HP').textContent = HPEnemy3;
+        infoEnemy2.style.width = `${sizeEnemy2}px`;
+        infoEnemy2.style.height = `${sizeEnemy2}px`;
+        infoEnemy2.style.top = `${groundPosEnemy2}px`;
+        infoEnemy2.src = enemyImg1;
+    infoEnemy3.textContent = HPEnemy3;
     if(HPEnemy3 < 1)
-        document.getElementById("enemy3").style.visibility = "hidden";
+        infoEnemy3.style.visibility = "hidden";
     else
-        document.getElementById("enemy3").style.visibility = "visible";
+        infoEnemy3.style.width = `${sizeEnemy3}px`;
+        infoEnemy3.style.height = `${sizeEnemy3}px`;
+        infoEnemy3.style.top = `${groundPosEnemy3}px`;
+        infoEnemy3.src = enemyImg1;
 
-    // TODO: These values will be hard coded to Gloombas for now, they should be updated with variables later down the road.
+    // TODO: Some values will be hard coded using the Gloomba enemy for now, they should be updated with more variables later down the road.
 }
 
 async function setGame()
 {
     HPMario = 25;
     HPVivian = 20;
+    HPEnemy1 = 10;
+
+    enemyGen(1, 1);
+
     update();
     await unveil();
     entrance("mario", 400);
+    if
+    enemytrance("enemy1", 500)
     await wait(300);
     entrance("vivian", 200);
     await wait(1000);
     hop("mario", 2);
     hop("vivian", 2);
+    hop("enemy1", 2);
     // ! Come back later!
 }
